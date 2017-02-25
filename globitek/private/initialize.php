ab5b042c9ab5b43d6c10c68786bcb3515edf4f23
Robert <rl78746@yahoo.com>
2017-02-24 20:45:30 -0600

    finished objective 2; added default msg value in pen tests sqli.php

---
+++ globitek/private/initialize.php
@@ -0,0 +1,40 @@
+<?php
+
+// Make sure output buffering is turned on in php.ini before
+// attempting page redirects. Or else uncomment the line below.
+ob_start();
+
+// Enable sessions
+// TODO add configurations
+$dayInSeconds = 1 * 24 * 60 * 60;//1day * 24hrs/1day * 60min/1hr * 60sec/1min
+$sessionConfigOptions = ['use_only_cookies' => true, 'cookie_lifetime' => $dayInSeconds, 'cookie_httponly' => true]; 
+session_start($sessionConfigOptions);
+
+// Turns off any browser built-in XSS protections
+// LEAVE THIS LINE IN WHILE YOU ARE LEARNING
+// We want to get punished for any XSS mistakes...
+header('X-XSS-Protection: 0');
+
+// Assign path shortcuts to PHP constants
+// __FILE__ returns the current path to this file
+// dirname() returns the path to the parent directory
+define("PRIVATE_PATH", dirname(__FILE__));
+define("PROJECT_PATH", dirname(PRIVATE_PATH));
+define("SHARED_PATH", PRIVATE_PATH . '/shared');
+define("PUBLIC_PATH", PROJECT_PATH . '/public');
+
+// DOC_ROOT is everything in URL up to and including "/public"
+$public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
+$doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
+define("DOC_ROOT", $doc_root);
+
+require_once('functions.php');
+require_once('database.php');
+require_once('query_functions.php');
+require_once('validation_functions.php');
+require_once('auth_functions.php');
+require_once('csrf_functions.php');
+
+$db = db_connect();
+
+?>
